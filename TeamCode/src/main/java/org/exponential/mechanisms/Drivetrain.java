976e8df2d6ce07d70001d50a6bfe2edec5cabe05
Eric Zhu <ezhu2009@gmail.com>
2020-09-30 10:40:51 -0400

    Made an Odometry class as an inner class to Drivetrain, reversed two drivetrain motors bc they weren't reversed before, put imu variable into drivetrain class

--- TeamCode/src/main/java/org/exponential/mechanisms/Drivetrain.java
+++ TeamCode/src/main/java/org/exponential/mechanisms/Drivetrain.java
@@ -3,28 +3,52 @@ package org.exponential.mechanisms;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.exponential.superclasses.Mechanism;
 
 import java.util.HashMap;
 
 public class Drivetrain implements Mechanism {
+
+    class Odometry {
+        DcMotorEx forwardLeftEnc;
+        DcMotorEx forwardRightEnc;
+        DcMotorEx horizontalEnc;
+
+
+        Odometry(DcMotorEx forwardLeftEnc, DcMotorEx forwardRightEnc, DcMotorEx horizontalEnc){
+            this.forwardLeftEnc = forwardLeftEnc;
+            this.forwardRightEnc = forwardRightEnc;
+            this.horizontalEnc = horizontalEnc;
+        }
+    }
+
     DcMotorEx frontLeft;
     DcMotorEx backLeft;
     DcMotorEx frontRight;
     DcMotorEx backRight;
-
-    @Override
+    IMU imu;
     public void initialize(LinearOpMode opMode) {
         frontLeft = opMode.hardwareMap.get(DcMotorEx.class, "frontLeft");
         backLeft = opMode.hardwareMap.get(DcMotorEx.class, "backLeft");
         frontRight = opMode.hardwareMap.get(DcMotorEx.class, "frontRight");
         backRight = opMode.hardwareMap.get(DcMotorEx.class, "backRight");
 
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // TODO: maybe not set to run with encoders if these encoders will be connected to odometry encoders
         frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = new IMU();
+        imu.initialize(opMode);
+
+
+
     }
 
     public void setPowerDriveMotors(HashMap<String, Double> powers) {
@@ -32,5 +56,7 @@ public class Drivetrain implements Mechanism {
         backLeft.setPower(powers.get("backLeft"));
         frontRight.setPower(powers.get("frontRight"));
         backRight.setPower(powers.get("backRight"));
+
+        
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SomeClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SomeClass.java
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode;
 public class SomeClass {
     Object SomeVariable;
     public static void eric() {
-        System.out.println("hello");
+        System.out.println("sigh");
     }
 }

1a100df216ecf783390413bf9a95bdb002719c15
yuhwanlee1 <yuhwanlee1@gmail.com>
2020-10-02 23:44:04 -0400

    putting variables at top of class

--- TeamCode/src/main/java/org/exponential/mechanisms/Drivetrain.java
+++ TeamCode/src/main/java/org/exponential/mechanisms/Drivetrain.java
@@ -11,6 +11,41 @@ import org.exponential.superclasses.Mechanism;
 import java.util.HashMap;
 
 public class Drivetrain implements Mechanism {
+    private DcMotorEx frontLeft;
+    private DcMotorEx backLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx backRight;
+    private IMU imu;
+    private LinearOpMode opMode;
+
+    public void initialize(LinearOpMode opMode) {
+        frontLeft = opMode.hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backLeft = opMode.hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontRight = opMode.hardwareMap.get(DcMotorEx.class, "frontRight");
+        backRight = opMode.hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // TODO: maybe not set to run with encoders if these encoders will be connected to odometry encoders
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = new IMU();
+        imu.initialize(opMode);
+
+        this.opMode = opMode;
+
+    }
+
+    public void setPowerDriveMotors(HashMap<String, Double> powers) {
+        frontLeft.setPower(powers.get("frontLeft"));
+        backLeft.setPower(powers.get("backLeft"));
+        frontRight.setPower(powers.get("frontRight"));
+        backRight.setPower(powers.get("backRight"));
+    }
 
     class Odometry implements Runnable {
         DcMotorEx forwardLeftEnc;
@@ -40,41 +75,5 @@ public class Drivetrain implements Mechanism {
 
 
         }
-    }
-
-    DcMotorEx frontLeft;
-    DcMotorEx backLeft;
-    DcMotorEx frontRight;
-    DcMotorEx backRight;
-    IMU imu;
-    LinearOpMode opMode;
-
-    public void initialize(LinearOpMode opMode) {
-        frontLeft = opMode.hardwareMap.get(DcMotorEx.class, "frontLeft");
-        backLeft = opMode.hardwareMap.get(DcMotorEx.class, "backLeft");
-        frontRight = opMode.hardwareMap.get(DcMotorEx.class, "frontRight");
-        backRight = opMode.hardwareMap.get(DcMotorEx.class, "backRight");
-
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
-
-        // TODO: maybe not set to run with encoders if these encoders will be connected to odometry encoders
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        imu = new IMU();
-        imu.initialize(opMode);
-
-        this.opMode = opMode;
-
-    }
-
-    public void setPowerDriveMotors(HashMap<String, Double> powers) {
-        frontLeft.setPower(powers.get("frontLeft"));
-        backLeft.setPower(powers.get("backLeft"));
-        frontRight.setPower(powers.get("frontRight"));
-        backRight.setPower(powers.get("backRight"));
     }
 }

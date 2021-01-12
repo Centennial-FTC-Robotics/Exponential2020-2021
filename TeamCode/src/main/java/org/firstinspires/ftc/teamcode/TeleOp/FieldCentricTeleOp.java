d5ffa688e93a47b7acc990435c1e1576a11cb5ae
ScrewInThePID <johnliuchs2020@gmail.com>
2021-01-11 23:58:34 -0500

    pls halp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.exponential.mechanisms.Turret;
 import org.exponential.robots.OurRobot;
 
 import java.util.HashMap;
@@ -12,6 +13,8 @@ import java.util.Map;
 public class FieldCentricTeleOp extends LinearOpMode {
     private double initialAngle;
     private double currentAngle;
+    private int targetNumber = 1;//John added this
+    private double [] goalXCoords = {85.3, 98.42, 111.54}; //Change these up please (I'll do it)
     private OurRobot ourRobot;
 
     @Override
@@ -26,6 +29,13 @@ public class FieldCentricTeleOp extends LinearOpMode {
 
             double inputX = gamepad1.left_stick_x;
             double inputY = -gamepad1.left_stick_y;
+
+            //John's targeting thing
+            if (gamepad2.a == true){
+                targetNumber = targetNumber + 1;
+            }
+            Turret.moveTurret(toggleTarget(targetNumber));//TODO Eric pls fix this
+            //--------------
 
             double theta = currentAngle - initialAngle;
             double rotatedX = inputX * Math.cos(theta) - inputY * Math.sin(theta);
@@ -64,5 +74,11 @@ public class FieldCentricTeleOp extends LinearOpMode {
             }
         }
         return powers;
+    }
+
+    //John's targeting method
+    public double toggleTarget(double targetXCoords){
+        targetXCoords = goalXCoords [targetNumber];
+        return targetXCoords;
     }
 }

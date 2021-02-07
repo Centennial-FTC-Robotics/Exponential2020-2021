453bdeef2793f7fa769b6b42c55c2a27829e6195
RickyWang718 <ezhu2009@gmail.com>
2021-02-07 15:52:21 -0500

    Diego, test this

--- TeamCode/src/main/java/org/exponential/Tests/turretTest.java
+++ TeamCode/src/main/java/org/exponential/Tests/turretTest.java
@@ -5,37 +5,39 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.exponential.robots.OurRobot;
+
 @TeleOp
 public class turretTest extends LinearOpMode {
     OurRobot expo = new OurRobot();
+
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
 
         expo.initialize(this);
-        expo.turret.turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        expo.turret.turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        turretPower(.2);
-        expo.turret.turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        double targetX = 0, targetY = 72;
+
+        expo.turret.setTarget(targetX, targetY);
+        expo.shooter.setPower(1);
 
         while (opModeIsActive()) {
-        turretPower(0);
-        if(gamepad1.a) {
-            turretPosition(1000);
-            turretPower(1);
-        }
-        if(gamepad1.b) {
-            turretPosition(0);
-            turretPower(1);
-        }
-            telemetry.addData("turretPosition", expo.turret.turretMotor.getCurrentPosition());
+            expo.odometry.update();
+            expo.turret.readjustTurretAngle();
+            if (gamepad1.a) {
+                expo.loader.loadAndUnload();
+            }
+            telemetry.addData("Current Position: ", "(" + expo.odometry.getxPos() + ", " + expo.odometry.getyPos() + ")");
+            telemetry.addData("Pointing Towards: ", "(" + targetX + ", " + targetY + ")");
+            telemetry.addData("Turret Angle: ", expo.turret.currentAngle);
             telemetry.update();
         }
     }
-    public void turretPower (double power) {
+
+    public void turretPower(double power) {
         expo.turret.turretMotor.setPower(power);
     }
-    public void turretPosition (int position) {
+
+    public void turretPosition(int position) {
         expo.turret.turretMotor.setTargetPosition(position);
     }
 }

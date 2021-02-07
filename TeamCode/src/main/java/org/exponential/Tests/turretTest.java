90344a6ff026c6e9d8fe6c3a80d8cb0c144b3c67
RickyWang718 <ezhu2009@gmail.com>
2021-02-07 15:55:03 -0500

    Scratch that, test this instead

--- TeamCode/src/main/java/org/exponential/Tests/turretTest.java
+++ TeamCode/src/main/java/org/exponential/Tests/turretTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.exponential.mechanisms.Turret;
 import org.exponential.robots.OurRobot;
 
 @TeleOp
@@ -19,12 +20,21 @@ public class turretTest extends LinearOpMode {
 
         expo.turret.setTarget(targetX, targetY);
         expo.shooter.setPower(1);
+        expo.turret.pointAtTarget();
 
         while (opModeIsActive()) {
             expo.odometry.update();
             expo.turret.readjustTurretAngle();
             if (gamepad1.a) {
                 expo.loader.loadAndUnload();
+            }
+            if(gamepad1.b){
+                if(expo.turret.currentCommand== Turret.RELOAD){
+                    expo.turret.pointAtTarget();
+                } else {
+                    expo.turret.pointToReloadPosition();
+                }
+                sleep(500);
             }
             telemetry.addData("Current Position: ", "(" + expo.odometry.getxPos() + ", " + expo.odometry.getyPos() + ")");
             telemetry.addData("Pointing Towards: ", "(" + targetX + ", " + targetY + ")");

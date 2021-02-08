baf647145dfb8ae1476121d376f630166dbb92c5
RickyWang718 <ezhu2009@gmail.com>
2021-02-07 21:01:52 -0500

    John, take this

---
+++ TeamCode/src/main/java/org/exponential/Tests/TurretTester.java
@@ -0,0 +1,15 @@
+package org.exponential.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.exponential.robots.OurRobot;
+
+public class TurretTester extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        OurRobot expo = new OurRobot();
+
+
+    }
+}
